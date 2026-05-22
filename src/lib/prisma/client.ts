8ad7ecbff27205f63cbdef052762761eafe9f7a5
meladiezoukahN1 <meladiezoukahN1@example.com>
2026-05-22 14:08:23 +0200

    fix src/lib/prisma/client.ts again

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -18,20 +18,27 @@
 //   });
 
 // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined");
+function createPrismaAdapter() {
+  const databaseUrl = process.env.DATABASE_URL?.trim();
+
+  if (!databaseUrl) {
+    throw new Error("DATABASE_URL is not defined");
+  }
+
+  return new PrismaMariaDb(databaseUrl);
 }
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter: createPrismaAdapter(),
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
